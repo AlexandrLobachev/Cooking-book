f9c227dd7d80136093d710ba671988318da744af
AlexandrLobachev <alexandrlobachev@gmail.com>
2024-02-01 01:21:47 +0300

    modify media

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -119,7 +119,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'collected_static'
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = '/media'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 DJOSER = {
     'LOGIN_FIELD': 'email',
